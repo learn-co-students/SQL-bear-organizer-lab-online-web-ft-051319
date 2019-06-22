100d79935d4954c2d2653be3d28ce698011f37fb
Alethea Cary <aletheacary@gmail.com>
2019-06-22 03:40:55 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+
+-- -- CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INT, gender TEXT, color TEXT, temperament TEXT, alive NUMERIC);
+
+
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
